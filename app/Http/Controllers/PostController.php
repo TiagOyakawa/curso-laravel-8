6bf030feb6a0fbceeec49a23e3e95e278aefcdf6
Tiago Oyakawa <tiago.oyakawa@devinsights.com.br>
2022-02-02 16:24:25 -0300

    Posts criado com sucesso

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,19 @@ class PostController extends Controller
     {
         $posts = Post::get();
 
-
         return view('admin.posts.index', compact('posts'));
     }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        Post::create($request->all());
+        
+        return redirect()->route('posts.index');
+    }
+
 }
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,3 +1,5 @@
+<a href="{{ route('posts.create') }}">Criar Novo Post</a>
+
 <h1>Posts</h1>
 
 @foreach ($posts as $post)
